d6d13bb4f2f6f7fa4e371d8f8163ff6273ada059
Rohit Parit <rohitparit1934@gmail.com>
2025-11-20 23:46:23 +0530

    Start

---
+++ src/main/java/com/rps/servicecenter/model/ServiceRequest.java
@@ -0,0 +1,4 @@
+package com.rps.servicecenter.model;
+
+public class ServiceRequest {
+}
---
+++ src/test/java/com/rps/servicecenter/ServiceCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rps.servicecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
